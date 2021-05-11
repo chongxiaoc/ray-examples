858c9169b18f50cbe244feb78cbc414b2730fd61
Chongxiao Cao <chongxiaoc@uber.com>
2021-05-11 17:43:58 -0500

    fix worker assignment

--- horovod/ray_start.py
+++ horovod/ray_start.py
@@ -1,15 +1,15 @@
 from horovod.ray import RayExecutor
+import horovod.torch as hvd
 import ray
 
 # Start the Ray cluster or attach to an existing Ray cluster
 ray.init()
 
-num_hosts = 4
-num_slots = 1
+num_workers = 4
 
 # Start num_hosts * num_slots actors on the cluster
 settings = RayExecutor.create_settings(timeout_s=30)
-executor = RayExecutor(settings, num_hosts=num_hosts, num_slots=num_slots, use_gpu=True)
+executor = RayExecutor(settings, num_workers=num_workers, use_gpu=True)
 
 # Launch the Ray actors on each machine
 # This will launch `num_slots` actors on each machine
@@ -24,6 +24,6 @@ def simple_fn():
 # Execute the function on all workers at once
 result = executor.run(simple_fn)
 # Check that the rank of all workers is unique
-assert len(set(result)) == hosts * num_slots
+assert len(set(result)) == num_workers
 
 executor.shutdown()
